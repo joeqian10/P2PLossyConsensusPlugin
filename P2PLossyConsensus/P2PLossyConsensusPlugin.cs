da4693bfaa4bc5373ed943008fb5669384c53d03
zhuoqian <zhuoqian10@gmail.com>
2019-06-13 13:44:49 +0800

    first commit

---
+++ P2PLossyConsensus/P2PLossyConsensusPlugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Neo.Network.P2P;
+using Neo.Network.P2P.Payloads;
+using Neo.Consensus;
+
+namespace Neo.Plugins
+{
+    public class P2PLossyConsensusPlugin : Plugin, IP2PPlugin
+    {
+        private static Random random = new Random();
+        public override void Configure()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public bool OnP2PMessage(Message message)
+        {
+            return true;
+        }
+
+        public bool OnConsensusMessage(ConsensusPayload payload)
+        {
+            if (payload.ConsensusMessage.Type == ConsensusMessageType.PrepareResponse)
+            {
+                return random.Next() % 2 == 0; // 50% loss
+            }
+            return true;
+        }
+    }
+}
